0d2290fa29754825c6bd6ac1b8252b947de2d4bd
Imeanabhi <abhi.nagabothu@gmail.com>
2024-06-06 18:51:53 +0530

    A

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Todolist from './Todolist.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    const[task,setTask]=useState("")
+    //stored in constant variable and set task variable used for resetting the task//
+    //for putting all the things in a list//
+
+    const [todos,setTodos]=useState("[]")
+    const newtodos=[...todos,task];
+    settodos(newtodos);
+    setTask("");
+
+    const changeHandler=e=>{
+    setTask(e.target.value)
+    }
+    const deleteHandler=(indexValue) => {
+        const newtodos = todos.filter((todo,index) => index!=indexValue);
+        Settodos(newtodos);
+    }
+
+    const submitHandler=e=>{
+    e.preventDefault();
+
+    console.log(task);
+    //console.log is used for checking whether the task the working properly or not//
+    }
+    return (
+    <div>
+        <center>
+        <div className="card">
+        < div className="card-body">
+            <h5 className="card-title">Todo Management Application</h5>
+            <form onSubmit={submitHandler}>
+                <input size="30" type="text" name="task" value={task} onChange={changeHandler} /> &nbsp;&nbsp;
+                <input type="submit" value="Add" name='add'/>
+            </form>
+            <Todolist todolist={todos}/>
+            </div>
+        </div>
+        </center>
     </div>
-  );
+    )
 }
 
-export default App;
+export default App
